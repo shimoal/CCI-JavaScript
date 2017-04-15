5a69ac3b68a95ccd0e99f19c5fdea40a4916d628
Alison Reed <alison.michelle.reed@gmail.com>
2017-04-14 21:55:18 -0700

    Add package json and test file to chapter One

---
+++ chapterOne/test.js
@@ -0,0 +1,34 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Problem One', function() {
+
+});
+
+describe('Problem Two', function() {
+
+});
+
+describe('Problem Three', function() {
+
+});
+
+describe('Problem Four', function() {
+
+});
+
+describe('Problem Five', function() {
+
+});
+
+describe('Problem Six', function() {
+
+});
+
+describe('Problem Seven', function() {
+
+});
+
+describe('Problem Eight', function() {
+
+});
