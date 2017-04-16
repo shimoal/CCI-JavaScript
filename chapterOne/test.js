e77e55bcad3f192a15c1c416d5c9c548eb77aa40
Alison Reed <alison.michelle.reed@gmail.com>
2017-04-15 21:05:23 -0700

    Wrote tests for problems 5 and 6

--- chapterOne/test.js
+++ chapterOne/test.js
@@ -1,5 +1,7 @@
 var chai = require('chai');
 var expect = chai.expect;
+var oneAway = require('./problem5.js');
+var stringCompression = require('./problem6.js');
 
 describe('Problem One', function() {
 
@@ -17,12 +19,40 @@ describe('Problem Four', function() {
 
 });
 
-describe('Problem Five', function() {
+describe('5. oneAway', function() {
+  it ('should be a function', function(){
+    expect(oneAway).to.be.a('function');
+  });
 
+  it('should return true for strings that are an insert away', function() {
+    expect(oneAway('pale', 'ple')).to.be.true;
+  });
+
+  it('should return true for strings that are a deletion away', function() {
+    expect(oneAway('pales', 'pale')).to.be.true;
+  });
+
+  it('should return true for strings that are a replacement away', function() {
+    expect(oneAway('pale', 'bale')).to.be.true;
+  });
+
+  it('should return false for strings that are more than one edit away', function() {
+    expect(oneAway('pale', 'bake')).to.be.false;
+  });
 });
 
-describe('Problem Six', function() {
+describe('6. stringCompression', function() {
+  it('should be a function', function(){
+    expect(stringCompression).to.be.a('function');
+  });
 
+  it('should return a compressed string', function() {
+    expect(stringCompression('aaabcccccaaa')).to.equal('a3b1c5a3');
+  });
+
+  it('should return the origin string if shorter than the compressed string', function() {
+    expect(stringCompression('abcde')).to.equal('abcde');
+  });
 });
 
 describe('Problem Seven', function() {
